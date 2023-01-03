a4cae144892fddfbdd7a43a5e300ccd82aeb2e74
Santiago Burgueño <santibur@live.com>
2023-01-03 19:29:11 +0000

    Added

--- src/app.js
+++ src/app.js
@@ -9,32 +9,33 @@ window.onload = function() {
   genero();
   document.getElementById("myBtn").addEventListener("click", function() {
     altanc();
+    borro();
     genero();
   });
 };
-
-let genero = function() {
+let borro = function() {
   const box = document.getElementById("header");
   box.className = "";
   const box2 = document.getElementById("footer");
   box2.className = "";
-
-  let tipo = clase();
-  let numcard = numero(tipo);
-
-  document.getElementById("header").classList.add(tipo);
-  document.getElementById("footer").classList.add(tipo);
-  document.getElementById("numero").innerHTML = numcard;
 };
-
+let genero = function() {
+  borro();
+  let tipo = clase();
+  var element = document.getElementById("header");
+  element.classList.add(tipo);
+  var element2 = document.getElementById("footer");
+  element2.classList.add(tipo);
+  document.getElementById("numero").innerHTML = numero(tipo);
+};
 let clase = function() {
   let rand = Math.floor(Math.random() * 4);
   let suit = ["spades", "clubs", "diamonds", "hearts"];
   return suit[rand];
 };
 
-let numero = function(tipo) {
-  if (tipo == "diamonds" || tipo == "hearts") {
+let numero = function(genero) {
+  if (genero == "diamonds" || genero == "hearts") {
     document.querySelector("h1").style.color = "red";
   } else {
     document.querySelector("h1").style.color = "black";
@@ -55,9 +56,19 @@ let numero = function(tipo) {
     return numcard;
   }
 };
-
-setTimeout(genero, 10000);
-
+setInterval(genero, 10000);
+var timeleft = 9;
+var timer = setInterval(function() {
+  if (timeleft <= 0) {
+    document.getElementById("countdown").innerHTML =
+      timeleft + " seconds remaining";
+    timeleft = 10;
+  } else {
+    document.getElementById("countdown").innerHTML =
+      timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
 let altanc = function() {
   let ancho = document.getElementById("width1").value;
   let alto = document.getElementById("height1").value;
